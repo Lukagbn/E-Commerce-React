3964f557ec9e6f42a16353464fed22242faaeb28
lukagbn <luka68690@gmail.com>
2026-02-20 17:29:47 +0400

    refactor code structure for navbar component and change styles

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -7,34 +7,38 @@ import layout from "@/app/layout.module.scss";
 async function Navbar() {
   return (
     <header>
-      <div className={styles.signUp}>
+      <div className={styles.promoBar}>
         <p>
           Sign up and get 20% off to your first order.{" "}
           <Link href={`#`}>Sign Up Now</Link>
         </p>
       </div>
-      <div className={`${styles.navHeader}  ${layout.innerContainer}`}>
-        <div className={styles.logoContainer}>
-          <Image
-            src={"/burgerIcon.svg"}
-            width={24}
-            height={24}
-            alt="burdger icon"
-          />
+      <div className={`${styles.navbar}  ${layout.innerContainer}`}>
+        <div className={styles.brand}>
+          <button className={styles.burgerBtn} aria-label="Open menu">
+            <Image
+              src="/burgerIcon.svg"
+              width={24}
+              height={24}
+              alt="burger icon"
+            />
+          </button>
           <Link href={"#"}>
-            <span className={styles.navLogo}>SHOP.CO</span>
+            <span className={styles.logo}>SHOP.CO</span>
           </Link>
         </div>
-        <nav className={`${styles.navigation}`}>
-          <ul className={styles.navigationList}>
+        <nav className={`${styles.nav}`}>
+          <ul className={styles.navList}>
             <li className={styles.dropDown}>
-              Shop
-              <Image
-                src={"/dropDown.png"}
-                width={16}
-                height={16}
-                alt="drop down"
-              />
+              <button className={styles.dropdownTrigger}>
+                Shop
+                <Image
+                  src="/dropDown.png"
+                  width={16}
+                  height={16}
+                  alt="drop down"
+                />
+              </button>
             </li>
             <li>
               <Link href={"#"}>On Sale</Link>
@@ -46,35 +50,39 @@ async function Navbar() {
               <Link href={"#"}>Bands</Link>
             </li>
           </ul>
-          <input type="search" placeholder="search" />
-          <div className={styles.iconContainer}>
-            <Link href={"#"}>
-              <Image
-                className={styles.searchIcon}
-                src={"/search.svg"}
-                width={24}
-                height={24}
-                alt="search icon"
-              ></Image>
-            </Link>
-            <Link href={"#"}>
-              <Image
-                src={"/cart.svg"}
-                width={24}
-                height={24}
-                alt="cart icon"
-              ></Image>
-            </Link>
-            <Link href={"#"}>
-              <Image
-                src={"/profile.svg"}
-                width={24}
-                height={24}
-                alt="profile icon"
-              ></Image>
-            </Link>
-          </div>
         </nav>
+        <div className={styles.actions}>
+          <input
+            type="search"
+            placeholder="search"
+            className={styles.searchInput}
+          />
+          <Link href={"#"}>
+            <Image
+              className={styles.searchIcon}
+              src={"/search.svg"}
+              width={24}
+              height={24}
+              alt="search icon"
+            ></Image>
+          </Link>
+          <Link href={"#"}>
+            <Image
+              src={"/cart.svg"}
+              width={24}
+              height={24}
+              alt="cart icon"
+            ></Image>
+          </Link>
+          <Link href={"#"}>
+            <Image
+              src={"/profile.svg"}
+              width={24}
+              height={24}
+              alt="profile icon"
+            ></Image>
+          </Link>
+        </div>
       </div>
     </header>
   );
