a8b081a77fd861a156332be7d2484d375967fbbb
lukagbn <luka68690@gmail.com>
2026-02-20 15:13:27 +0400

    add desktop responsive styles for navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ async function Navbar() {
           <Link href={`#`}>Sign Up Now</Link>
         </p>
       </div>
-      <div className={styles.navHeader}>
+      <div className={`${styles.navHeader}  ${layout.innerContainer}`}>
         <div className={styles.logoContainer}>
           <Image
             src={"/burgerIcon.svg"}
@@ -25,34 +25,56 @@ async function Navbar() {
             <span className={styles.navLogo}>SHOP.CO</span>
           </Link>
         </div>
-        <div className={styles.navigation}>
-          <span>
+        <nav className={`${styles.navigation}`}>
+          <ul className={styles.navigationList}>
+            <li>
+              Shop
+              <Image
+                src={"/dropDown.png"}
+                width={16}
+                height={16}
+                alt="drop down"
+              />
+            </li>
+            <li>
+              <Link href={"#"}>On Sale</Link>
+            </li>
+            <li>
+              <Link href={"#"}>New Arrivals</Link>
+            </li>
+            <li>
+              <Link href={"#"}>Bands</Link>
+            </li>
+          </ul>
+          <input type="search" placeholder="search" />
+          <div className={styles.iconContainer}>
             <Link href={"#"}>
               <Image
+                className={styles.searchIcon}
                 src={"/search.svg"}
                 width={24}
                 height={24}
                 alt="search icon"
               ></Image>
             </Link>
-          </span>
-          <Link href={"#"}>
-            <Image
-              src={"/cart.svg"}
-              width={24}
-              height={24}
-              alt="cart icon"
-            ></Image>
-          </Link>
-          <Link href={"#"}>
-            <Image
-              src={"/profile.svg"}
-              width={24}
-              height={24}
-              alt="profile icon"
-            ></Image>
-          </Link>
-        </div>
+            <Link href={"#"}>
+              <Image
+                src={"/cart.svg"}
+                width={24}
+                height={24}
+                alt="cart icon"
+              ></Image>
+            </Link>
+            <Link href={"#"}>
+              <Image
+                src={"/profile.svg"}
+                width={24}
+                height={24}
+                alt="profile icon"
+              ></Image>
+            </Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
