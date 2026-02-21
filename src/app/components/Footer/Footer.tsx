7d443dbaffad7bae1078e3671fcced631870a8c1
lukagbn <luka68690@gmail.com>
2026-02-21 15:18:42 +0400

    reafactor footer styles

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,7 +4,21 @@ import Image from "next/image";
 import layout from "@/app/layout.module.scss";
 import Link from "next/link";
 function Footer() {
-  const socialList = [
+  type FooterSection = {
+    title: string;
+    paragraphs: string[];
+    link?: string;
+  };
+  type PaymentSection = {
+    img: string;
+    alt: string;
+  };
+  type SocialLink = {
+    img: string;
+    alt: string;
+    className: string;
+  };
+  const socialLiks: SocialLink[] = [
     {
       img: "/twitter.svg",
       alt: "twitter icon",
@@ -26,15 +40,6 @@ function Footer() {
       className: "github",
     },
   ];
-  type FooterSection = {
-    title: string;
-    paragraphs: string[];
-    link?: string;
-  };
-  type PaymentSection = {
-    img: string;
-    alt: string;
-  };
   const footerSection: FooterSection[] = [
     {
       title: "COMPANY",
@@ -88,60 +93,60 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.footerForm} ${layout.innerContainer}`}>
-        <form>
+      <div className={`${styles.footerTop} ${layout.innerContainer}`}>
+        <form className={styles.newsletterForm}>
           <h2>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h2>
-          <div className={styles.contentContainer}>
-            <div className={styles.formGroup}>
+          <div className={styles.newsletterControls}>
+            <div className={styles.inputGroup}>
               <input type="email" placeholder="Enter your email address" />
               <img
                 src="/emailIcon.svg"
                 alt="email icon"
-                className={styles.emailIcon}
+                className={styles.inputIcon}
               />
             </div>
             <button type="submit">Subscribe to Newsletter</button>
           </div>
         </form>
       </div>
-      <div className={`${styles.footerWrapper} ${layout.innerContainer}`}>
-        <div className={styles.logoBox}>
-          <h2 className={styles.footerLogo}>SHOP.CO</h2>
+      <div className={`${styles.footerMain} ${layout.innerContainer}`}>
+        <div className={styles.footerBrand}>
+          <h2 className={styles.logo}>SHOP.CO</h2>
           <p>
             We have clothes that suits your style and which you’re proud to
             wear. From women to men.
           </p>
-          <div className={styles.socialContainer}>
-            {socialList.map((item, index) => (
+          <div className={styles.socialLinks}>
+            {socialLiks.map((item) => (
               <div
                 className={`${styles.socialBox} ${item.className}`}
-                key={index}
+                key={item.className}
               >
                 <Image src={item.img} width={13} height={13} alt={item.alt} />
               </div>
             ))}
           </div>
         </div>
-        <div className={styles.footerSection}>
-          {footerSection.map((item, index) => (
-            <div className={styles.footerColumn} key={index}>
+        <div className={styles.footerNav}>
+          {footerSection.map((item) => (
+            <div className={styles.footerColumn} key={item.title}>
               <h4>{item.title}</h4>
-              {item.paragraphs.map((i, index) => (
-                <Link href={"#"} key={index}>
+              {item.paragraphs.map((i) => (
+                <Link href={"#"} key={i}>
                   {i}
                 </Link>
               ))}
             </div>
           ))}
         </div>
-        <div className={styles.rightsContainer}>
+        <div className={styles.footerBottom}>
           <hr />
           <p className={styles.rights}>
             Shop.co © 2000-{currentYear}, All Rights Reserved
           </p>
-          <div className={styles.paymentContainer}>
+          <div className={styles.paymentMethods}>
             {paymentSection.map((item, index) => (
-              <div className={styles.paymentBox} key={index}>
+              <div className={styles.paymentItem} key={index}>
                 <Image src={item.img} width={27} height={10} alt={item.alt} />
               </div>
             ))}
